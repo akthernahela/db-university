0e2def2e90bfcddbd83dab23c8877af298ccd372
akthernahela <nahela00@gmail.com>
2025-12-04 22:11:00 +0100

    Fourth task

--- file_homework.sql
+++ file_homework.sql
@@ -7,7 +7,10 @@ USE db_universityadmin;
 #SELECT * FROM `courses` WHERE `cfu` > 10;
 
 #3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT * FROM `students` WHERE (YEAR(CURDATE()) - YEAR(date_of_birth)) > 30;
+#SELECT * FROM `students` WHERE (YEAR(CURDATE()) - YEAR(date_of_birth)) > 30;
+
+#4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I semestre';
 
 
 
