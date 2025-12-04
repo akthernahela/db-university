f0315201ca706a527ad52430c73dc04f57ef1158
akthernahela <nahela00@gmail.com>
2025-12-04 22:20:46 +0100

    Fifth task done

--- file_homework.sql
+++ file_homework.sql
@@ -10,7 +10,11 @@ USE db_universityadmin;
 #SELECT * FROM `students` WHERE (YEAR(CURDATE()) - YEAR(date_of_birth)) > 30;
 
 #4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I semestre';
+#SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I semestre';
+
+#5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
+
 
 
 
