e622e986841894613a6302e25079b9a9f8f9762e
akthernahela <nahela00@gmail.com>
2025-12-05 00:04:16 +0100

    Group third task

--- file_homework.sql
+++ file_homework.sql
@@ -29,5 +29,9 @@ USE db_universityadmin;
 #1. Contare quanti iscritti ci sono stati ogni anno
 #SELECT YEAR(enrolment_date) AS `enrolment_year`, COUNT(*) FROM `students` GROUP BY YEAR(enrolment_date) ORDER BY `enrolment_year`;
 
-#2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT `office_address`, COUNT(*) AS `teacher` FROM `teachers` GROUP BY `office_address`;
+#2. Contare gli insegnanti che hanno l^ufficio nello stesso edificio
+#SELECT `office_address`, COUNT(*) AS `teacher` FROM `teachers` GROUP BY `office_address`;
+
+#3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`, AVG(vote) AS `avrg_marks` FROM `exam_student` GROUP BY `exam_id`;
+
